37580755dfdcfc9339c2b53c0476a8cc765b54d0
emeraldjecko <shirluv.amoguis@gmail.com>
2024-09-25 15:51:34 +0800

    Check if feature is enabled

---
+++ Exercise2/UserSettings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class UserSettings
+{
+    public static bool IsFeatureEnabled(string settings, int index)
+    {
+        if (settings.Length != 8 || index < 1 || index > 8)
+        {
+            throw new ArgumentException("Invalid settings or settings index");
+        }
+
+        if (settings[index - 1] == '1')
+            return true;
+        else return false;
+    }
+
+    public static void Main()
+    {
+        //Test settings
+        string userSettings = "00000000";
+        int index = 7;
+
+        bool isFeatureEnabled = IsFeatureEnabled(userSettings, index);
+        Console.WriteLine(isFeatureEnabled);
+
+        userSettings = "00000010";
+        index = 7;
+
+        isFeatureEnabled = IsFeatureEnabled(userSettings, index);
+        Console.WriteLine(isFeatureEnabled);
+
+        userSettings = "11111111";
+        index = 4;
+
+        isFeatureEnabled = IsFeatureEnabled(userSettings, index);
+        Console.WriteLine(isFeatureEnabled);
+
+    }
+}
